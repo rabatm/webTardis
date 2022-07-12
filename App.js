b1aa209be85ce268bda98500fccfa05e3946cda5
Rabat martin <martin.rabat@gmail.com>
2022-07-12 10:04:09 +0200

    ajout du login et de l'image

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './components/Login';
 
 export default function App() {
+  const [notLog,setNotLog]= useState(true)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {notLog ?
+      <Login login={() => setNotLog(false) } />
+      : <View>
+        <Text>Loggggéééééé</Text>
+      </View>
+      }
     </View>
   );
 }
