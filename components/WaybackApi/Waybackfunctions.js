a58110fdf4ae824cd169eebe2376327905f3fd26
Rabat martin <martin.rabat@gmail.com>
2022-07-12 17:24:20 +0200

    Remove Axio

--- components/WaybackApi/Waybackfunctions.js
+++ components/WaybackApi/Waybackfunctions.js
@@ -4,19 +4,16 @@ const WAYBACK_URL ='https://archive.org/wayback/available'
 
 export async function getInfos (urlSite,timestamp) {
   let infos={url:'',time:''}
-  console.log(timestamp)
+
   try {
-  const res = await axios.get(`${WAYBACK_URL}?url=${urlSite}&timestamp=${timestamp}`)
-
-  if (res.data.archived_snapshots )
-  {   
-    infos={url : res.data.archived_snapshots.closest.url, time : res.data.archived_snapshots.closest.timestamp}
+    const response = await fetch(
+      `${WAYBACK_URL}?url=${urlSite}&timestamp=${timestamp}`
+    );
+    const json = await response.json();
+    infos={url : json.archived_snapshots.closest.url, time : json.archived_snapshots.timestamp}
+    return infos
+  } catch (error) {
+    console.error(error);
   }
-  }
-  catch {
-    console.error('no info : from API')
-  }
-
-  return infos
 }
 
